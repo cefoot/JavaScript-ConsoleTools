fa68c125d3f46f7fe0fe006bfc0c31407bd4a137
cefoot <chrisericpape+github@gmail.com>
2021-11-14 09:32:52 +0100

    Update AmazonMusicDownload.js

--- AmazonMusicDownload.js
+++ AmazonMusicDownload.js
@@ -1,28 +1,28 @@
 // Downloading mp3 files from amazon music page (test on page 11.2021 https://music.amazon.de/recently/purchased)
-var btns = document.getElementsByTagName("music-image-row");
+//start index (from webpage)
 var musicIdx = 1;
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function download(){
-	var itms = document.getElementsByTagName("music-image-row");
+	var itms = document.getElementsByTagName("music-image-row"); //get current list of songs (changes when scrolling)
 	var itm = itms[0]
 	for(var i = 0; i < itms.length; i++){
-		if(itms[i].innerText.startsWith(""+musicIdx)){
+		if(itms[i].innerText.startsWith(""+musicIdx)){ //find desired song (text starts with index)
 			itm = itms[i];
 			break;
 		}
 	}
 	console.log("Music:"+musicIdx);
 	console.log(itm.attributes["primary-text"].value);
-    itm.scrollIntoView();
+    itm.scrollIntoView(); //scroll song into view (eventually updates list in background)
 	await sleep(500);
-	itm.getElementsByTagName("music-button")[2].click()
+	itm.getElementsByTagName("music-button")[2].click(); // click menu button
 	await sleep(500);
-	document.getElementsByTagName("music-list-item")[4].click()
+	document.getElementsByTagName("music-list-item")[4].click(); // click download button
 	await sleep(500);
-	document.getElementById("dialogButton1").click()
-	if(musicIdx<80){
+	document.getElementById("dialogButton1").click(); //click download direct button in dialog
+	if(musicIdx<80){ // <-- end index (from webpage)
 		setTimeout(download, 1000);
 	}
 	musicIdx++;
